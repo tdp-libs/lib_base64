5005a511be74498a471e67ae24db4b916558e742
Tom Paynter <tompaynter@tdpe.co.uk>
2019-06-10 14:57:03 +0100

    Refactoring and linting.

--- inc/base64.h
+++ inc/base64.h
@@ -8,7 +8,7 @@
 
 #include <string>
 
-std::string base64_encode(unsigned char const* , size_t len);
+std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len);
 std::string base64_encode(const std::string& s);
 
 std::string base64_decode(std::string const& s);
