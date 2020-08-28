e7dbca8e02e7ba61db173146349ec87f5488102d
Tom Paynter <tompaynter@tdpe.co.uk>
2020-08-28 12:41:33 +0100

    Working towards scenes.

--- src/base64.cpp
+++ src/base64.cpp
@@ -34,107 +34,116 @@
 
 namespace
 {
-const std::string base64_chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz"
-        "0123456789+/";
-
+const std::string& chars()
+{
+  static const std::string base64_chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz"
+      "0123456789+/";
+  return base64_chars;
+}
 
 bool is_base64(char c)
 {
-    return (isalnum(c) || (c == '+') || (c == '/'));
+  return (isalnum(c) || (c == '+') || (c == '/'));
 }
 }
 
 //##################################################################################################
-std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len) {
-    std::string ret;
-    int i = 0;
-    int j = 0;
-    unsigned char char_array_3[3];
-    unsigned char char_array_4[4];
+std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len)
+{
+  const auto& base64_chars = chars();
 
-    while (in_len--) {
-        char_array_3[i++] = *(bytes_to_encode++);
-        if (i == 3) {
-            char_array_4[0] = uint8_t((char_array_3[0] & 0xfc) >> 2);
-            char_array_4[1] = uint8_t(((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4));
-            char_array_4[2] = uint8_t(((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6));
-            char_array_4[3] = char_array_3[2] & 0x3f;
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
 
-            for(i = 0; (i <4) ; i++)
-                ret += base64_chars[char_array_4[i]];
-            i = 0;
-        }
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = uint8_t((char_array_3[0] & 0xfc) >> 2);
+      char_array_4[1] = uint8_t(((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4));
+      char_array_4[2] = uint8_t(((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6));
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
     }
+  }
 
-    if (i)
-    {
-        for(j = i; j < 3; j++)
-            char_array_3[j] = '\0';
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
 
-        char_array_4[0] = uint8_t(( char_array_3[0] & 0xfc) >> 2);
-        char_array_4[1] = uint8_t(((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4));
-        char_array_4[2] = uint8_t(((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6));
+    char_array_4[0] = uint8_t(( char_array_3[0] & 0xfc) >> 2);
+    char_array_4[1] = uint8_t(((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4));
+    char_array_4[2] = uint8_t(((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6));
 
-        for (j = 0; (j < i + 1); j++)
-            ret += base64_chars[char_array_4[j]];
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
 
-        while((i++ < 3))
-            ret += '=';
+    while((i++ < 3))
+      ret += '=';
 
-    }
+  }
 
-    return ret;
+  return ret;
 
 }
 
 //##################################################################################################
 std::string base64_encode(const std::string& s)
 {
-    return base64_encode(reinterpret_cast<const unsigned char*>(s.data()), s.size());
+  return base64_encode(reinterpret_cast<const unsigned char*>(s.data()), s.size());
 }
 
 //##################################################################################################
-std::string base64_decode(std::string const& encoded_string) {
-    size_t in_len = encoded_string.size();
-    size_t i = 0;
-    size_t j = 0;
-    size_t in_ = 0;
-    unsigned char char_array_4[4]={0,0,0,0};
-    unsigned char char_array_3[3]={0,0,0};
-    std::string ret;
+std::string base64_decode(std::string const& encoded_string)
+{
+  const auto& base64_chars = chars();
 
-    while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+  size_t in_len = encoded_string.size();
+  size_t i = 0;
+  size_t j = 0;
+  size_t in_ = 0;
+  unsigned char char_array_4[4]={0,0,0,0};
+  unsigned char char_array_3[3]={0,0,0};
+  std::string ret;
+
+  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+  {
+    char_array_4[i++] = uint8_t(encoded_string[in_]);
+    in_++;
+    if (i ==4)
     {
-        char_array_4[i++] = uint8_t(encoded_string[in_]);
-        in_++;
-        if (i ==4)
-        {
-            for (i = 0; i <4; i++)
-                char_array_4[i] = uint8_t(base64_chars.find(char(char_array_4[i])));
+      for (i = 0; i <4; i++)
+        char_array_4[i] = uint8_t(base64_chars.find(char(char_array_4[i])));
 
-            char_array_3[0] = uint8_t(( char_array_4[0] << 2       ) + ((char_array_4[1] & 0x30) >> 4));
-            char_array_3[1] = uint8_t(((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2));
-            char_array_3[2] = uint8_t(((char_array_4[2] & 0x3) << 6) +   char_array_4[3]);
+      char_array_3[0] = uint8_t(( char_array_4[0] << 2       ) + ((char_array_4[1] & 0x30) >> 4));
+      char_array_3[1] = uint8_t(((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2));
+      char_array_3[2] = uint8_t(((char_array_4[2] & 0x3) << 6) +   char_array_4[3]);
 
-            for (i = 0; (i < 3); i++)
-                ret += char(char_array_3[i]);
-            i = 0;
-        }
+      for (i = 0; (i < 3); i++)
+        ret += char(char_array_3[i]);
+      i = 0;
     }
+  }
 
-    if (i)
-    {
-        for (j = 0; j < i; j++)
-            char_array_4[j] = uint8_t(base64_chars.find(char(char_array_4[j])));
+  if (i)
+  {
+    for (j = 0; j < i; j++)
+      char_array_4[j] = uint8_t(base64_chars.find(char(char_array_4[j])));
 
-        char_array_3[0] = uint8_t((char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4));
-        char_array_3[1] = uint8_t(((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2));
+    char_array_3[0] = uint8_t((char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4));
+    char_array_3[1] = uint8_t(((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2));
 
-        for (j = 0; (j < i - 1); j++)
-            ret += char(char_array_3[j]);
-    }
+    for (j = 0; (j < i - 1); j++)
+      ret += char(char_array_3[j]);
+  }
 
-    return ret;
+  return ret;
 }
